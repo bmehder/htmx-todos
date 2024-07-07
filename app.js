9fce489a6a5fa940c2840a5f6fddb94828017fb5
bmehder <bmehder@gmail.com>
2024-07-07 18:40:35 +0200

    moved route handlers to model file

--- app.js
+++ app.js
@@ -1,44 +1,11 @@
 import express from 'express'
-import todos from './models/todos.js'
-import createViewApp from './views/index.js'
-import createViewForm from './views/form.js'
-import createViewList from './views/list.js'
+import routeHandlers from './models/todos.js'
 
 // Create server
 const app = express()
 
 // Add middleware to parse form data
 app.use(express.urlencoded({ extended: false }))
-
-// Route handlers
-const routeHandlers = {
-	getAppView: (_, res) => res.send(createViewApp()),
-	getTodos: (_, res) => res.send(createViewList(todos)),
-	getForm: (_, res) => res.send(createViewForm(todos)),
-	addTodo: (req, res) => {
-		const { text } = req.body
-
-		text && todos.push({ id: crypto.randomUUID(), text, complete: false })
-
-		res.redirect('/todos')
-	},
-	toggleTodoComplete: (req, res) => {
-		const { id } = req.params
-		const idx = todos.findIndex(todo => todo.id === id)
-
-		todos[idx] = { ...todos[idx], complete: !todos[idx].complete }
-
-		res.send(createViewList(todos))
-	},
-	deleteTodo: (req, res) => {
-		const { id } = req.params
-		const idx = todos.findIndex(todo => todo.id === id)
-
-		todos.splice(idx, 1)
-
-		res.send(createViewList(todos))
-	},
-}
 
 // Routes
 app.get('/', routeHandlers.getAppView)
@@ -54,6 +21,4 @@ app.put('/todos/:id', routeHandlers.toggleTodoComplete)
 app.delete('/todos/:id', routeHandlers.deleteTodo)
 
 //Listen on port 3000
-const listener = app.listen(3000, () => {
-	console.log('Listening on port ' + listener.address().port)
-})
+app.listen(3000, () => console.log('Listening on port 3000'))
--- models/todos.js
+++ models/todos.js
@@ -1,3 +1,8 @@
+import createViewApp from '../views/index.js'
+import createViewForm from '../views/form.js'
+import createViewList from '../views/list.js'
+
+// Data Model
 const todos = [
 	{
 		id: '1',
@@ -11,4 +16,39 @@ const todos = [
 	},
 ]
 
-export default todos
+// Route handlers
+const routeHandlers = {
+	getAppView: (_, res) => res.send(createViewApp()),
+
+	getTodos: (_, res) => res.send(createViewList(todos)),
+
+	getForm: (_, res) => res.send(createViewForm()),
+
+	addTodo: (req, res) => {
+		const { text } = req.body
+
+		text && todos.push({ id: crypto.randomUUID(), text, complete: false })
+
+		res.send(createViewList(todos))
+	},
+
+	toggleTodoComplete: (req, res) => {
+		const { id } = req.params
+		const idx = todos.findIndex(todo => todo.id === id)
+
+		todos[idx] = { ...todos[idx], complete: !todos[idx].complete }
+
+		res.send(createViewList(todos))
+	},
+
+	deleteTodo: (req, res) => {
+		const { id } = req.params
+		const idx = todos.findIndex(todo => todo.id === id)
+
+		todos.splice(idx, 1)
+
+		res.send(createViewList(todos))
+	},
+}
+
+export default routeHandlers
